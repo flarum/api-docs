0b8ff9a6c391caf615292b8d803570c161e5bd6e
David Sevilla Martín <dsevilla192@icloud.com>
2018-09-30 11:13:07 -0400

    php: remove boostrap js import

--- themes/flarum/src/js/index.js
+++ themes/flarum/src/js/index.js
@@ -3,8 +3,6 @@ import '../less/index.less';
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
-// import 'bootstrap/dist/js/bootstrap.min';
-import 'bootstrap/js/dist/collapse';
 
 import typeahead from './typeahead.0.11.1';
 

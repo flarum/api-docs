de58f5eb0644e33d147a2c9bfd2ed80849cdb412
David Sevilla Martin <dsevilla192@icloud.com>
2019-08-14 14:15:12 -0400

    Fix SHA not being set, combine build & deploy scripts into one

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,31 @@
 set -e
 
+echo -e "\e[36m\e[1mTriggered new API documentation build"
+
+# -> BUILD
+
+echo -e "\e[36m\e[1mChecking out 'master'"
+git checkout -qf master
+
+export ROOT_PATH=$(cd $GITHUB_WORKSPACE/.. && pwd)
+export REPO_FOLDER=${PWD##*/}
+FLARUM_FOLDER="$ROOT_PATH/flarum"
+
+echo -e "\e[36m\e[1mCloning flarum/core > $FLARUM_FOLDER"
+
+git clone https://github.com/flarum/core $FLARUM_FOLDER &> /dev/null
+
+echo -e "\e[36m\e[1m-> Using latest commit"
+
+SHA=$(cd $FLARUM_FOLDER && git rev-parse --verify HEAD --short)
+
+echo -e "\e[36m\e[1mBuilding for commit $SHA\n\n"
+
+# Build node src --php --js
+node src --php --js
+
+# -> DEPLOY
+
 # Commit + Push
 echo -e "\n\n\e[36m\e[1mSetting up git"
 

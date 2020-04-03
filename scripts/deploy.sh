e16716db37fde041cea4fbf76b2016fc2f10abdc
David Sevilla Martin <me@datitisev.me>
2020-04-03 14:43:33 -0400

    rework a bit of bash script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -43,12 +43,18 @@ node src --php --js
 # rewrite js
 echo -e "\e[36m\e[1mBuilding ds/frontend-framework-rewrite-mithril"
 
-(cd $FLARUM_FOLDER && git fetch origin && git checkout ds/frontend-framework-rewrite-mithril)
+cd $FLARUM_FOLDER
+git fetch origin
+git checkout ds/frontend-framework-rewrite-mithril
 SHA_REWRITE=$(getSHA)
-(cd $FLARUM_FOLDER/js && npm i --prefer-offline --no-audit)
-(cd $FLARUM_FOLDER/js && npx typedoc src --includeDeclarations --ignoreCompilerErrors --jsx true --out "$REPO_FOLDER/docs/js/ds~frontend-framework-rewrite-mithril" --excludeExternals --name 'Flarum API' --plugin typedoc-plugin-external-module-map --external-modulemap ".*js\/src\/([\\w\\-_]+(\/[\\w\\-_]+)?)\/"  --listInvalidSymbolLinks --hideGenerator)
+
+cd js
+npm i --prefer-offline --no-audit
+npx typedoc src --includeDeclarations --ignoreCompilerErrors --jsx true --out "$REPO_FOLDER/docs/js/ds~frontend-framework-rewrite-mithril" --excludeExternals --name 'Flarum API' --plugin typedoc-plugin-external-module-map --external-modulemap ".*js\/src\/([\\w\\-_]+(\/[\\w\\-_]+)?)\/"  --listInvalidSymbolLinks --hideGenerator
 
 # -> DEPLOY
+
+cd $REPO_FOLDER
 
 # Commit + Push
 echo -e "\n\n\e[36m\e[1mSetting up git"

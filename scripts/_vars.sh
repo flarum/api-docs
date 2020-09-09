5821c436eaa573cf0a6f9f7546c35b3aaf2cd4d0
David Sevilla Martin <me@datitisev.me>
2020-09-09 19:37:17 -0400

    Fix JS using wrong flarum folder path

--- scripts/_vars.sh
+++ scripts/_vars.sh
@@ -9,4 +9,6 @@ export SCRIPTS_PATH=$(dirname $0)
 export REPO_PATH=$(cd $SCRIPTS_PATH; cd ..; pwd)
 export FLARUM_PATH="${REPO_PATH}/flarum";
 
+echo -e "${style}Using flarum/core @ $FLARUM_PATH$reset"
+
 echo "::set-env name=FLARUM_SHA::$(cd $FLARUM_PATH && git rev-parse --verify HEAD --short)"

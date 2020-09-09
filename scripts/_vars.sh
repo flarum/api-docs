0d91ae11339016dada88bacf1a4fdc45336071da
David Sevilla Martin <me@datitisev.me>
2020-09-08 20:27:23 -0400

    Set github workflow FLARUM_SHA

--- scripts/_vars.sh
+++ scripts/_vars.sh
@@ -6,3 +6,5 @@ export reset='\e[0;10m'
 export SCRIPTS_PATH=$(dirname $0)
 export REPO_PATH=$(cd $SCRIPTS_PATH; cd ..; pwd)
 export FLARUM_PATH="${REPO_PATH}/flarum";
+
+echo "::set-env name=FLARUM_SHA::$(cd $FLARUM_PATH && git rev-parse --verify HEAD --short)"

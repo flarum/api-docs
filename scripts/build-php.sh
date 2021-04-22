73803ebdc2725e920ee8bfdf9b666a74355693b9
David Sevilla Martin <me@datitisev.me>
2021-04-21 20:32:10 -0400

    Update Doctum to v5.4.1, add '--force' to update command to re-render all PHP documentation

--- scripts/build-php.sh
+++ scripts/build-php.sh
@@ -4,7 +4,7 @@ source $(dirname $0)/_vars.sh
 
 echo -e "${style}Generating $reset"
 
-php "$REPO_PATH/doctum.phar" update doctum-config.php -v
+php "$REPO_PATH/doctum.phar" update doctum-config.php -v --force
 
 bash $SCRIPTS_PATH/set-redirects.sh php
 bash $SCRIPTS_PATH/set-index-file.sh php

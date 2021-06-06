debd07d7f7a7fafea72d85b25ca01d03fe256097
David Sevilla Martin <me@datitisev.me>
2021-06-06 15:39:10 -0400

    Fix redirects

--- scripts/set-redirects.sh
+++ scripts/set-redirects.sh
@@ -2,4 +2,4 @@
 
 echo -e "${style}Updating redirect $reset"
 
-sed -i "s/^\\/$1.*$/\\/$1\\/latest\t\t\t\\/$1\/$(cd $FLARUM_PATH; git tag | sort -V | tail -1)\\/index.html/" "$REPO_PATH/docs/_redirects"
+sed -i "s/^\\/$1.*$/\\/$1\\/latest\t\t\t\\/$1\/$(cd docs/$1; ls -d */ | cut -f1 -d'/' | sort -V | tail -1)\\/index.html/" "$REPO_PATH/docs/_redirects"

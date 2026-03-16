7c9ea016dc1054c0626be67b49c04c236d28fa6f
David Sevilla Martin <me@dsevilla.dev>
2026-03-16 14:45:44 -0400

    WIP more monorepo stuff

--- scripts/build-js.sh
+++ scripts/build-js.sh
@@ -12,7 +12,6 @@ generate () {
 
     ref=$1
     path="$REPO_PATH/docs/js/$ref"
-    # js_path="$FLARUM_CORE_PATH/js"
 
     if [ "$2" == "true" ] && [ -d "$path" ]; then
         return
@@ -24,23 +23,24 @@ generate () {
     mkdir $path
 
     cd $path
-    (cd $FLARUM_PATH && git checkout -q -- . && git checkout -q $ref)
+    (cd $FLARUM_PATH && git checkout -q -- . && git clean -f -d && git checkout -q $ref)
     (cd $FLARUM_PATH && yarn install --immutable)
 
     cp -v "$REPO_PATH/typedoc.package.json" "$FLARUM_CORE_PATH/js/typedoc.json"
+    sed -i '11d' "$FLARUM_CORE_PATH/js/typedoc.json"
     for i in $(echo "$FLARUM_PATH/extensions/*/js"); do        
         cp -v "$REPO_PATH/typedoc.package.json" "$i/typedoc.json"
         
         if ! [[ -f "$i/tsconfig.json" ]]; then
             cp "$FLARUM_PATH/extensions/flags/js/tsconfig.json" "$i/tsconfig.json"
-        else
-            sed -i "s+framework/core/js/dist-typings+framework/core/js/src+g" "$i/tsconfig.json"
         fi
+
+        # sed -i "s+framework/core/js/dist-typings+framework/core/js/src+g" "$i/tsconfig.json"
     done
 
     cd $REPO_PATH
     export NODE_OPTIONS="--max-old-space-size=16384"
-    npx typedoc --gitRevision $ref --out "$REPO_PATH/docs/js/$ref" --name "Flarum ($ref)" --readme "$REPO_PATH/src/readme-js.md" --skipErrorChecking 
+    npx typedoc --gitRevision $ref --out "$REPO_PATH/docs/js/$ref" --name "Flarum ($ref)" --readme "$REPO_PATH/src/readme-js.md"
 
     cd $REPO_PATH
 }
